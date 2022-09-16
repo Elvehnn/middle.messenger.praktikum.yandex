b86cce90ba5e673877ef18791440a95170f9cecc
Elena Shashina <elvehnn@yandex.ru>
2022-09-16 23:17:20 +0300

    fix: fixed PR comments

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Example app listening on port ${PORT}`);
 });
