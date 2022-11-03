445498506a80f201c31f1f5a41815a66c9abe784
Elena Shashina <elvehnn@yandex.ru>
2022-11-03 21:11:41 +0300

    fix: trying to fix displaying static images

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('dist'));
+app.use(express.static(`${__dirname}/dist`));
 
-app.get('*', (req, res) => {
-    res.sendFile(`${__dirname}/dist/index.html`);
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
