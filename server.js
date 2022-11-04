b6be1c7820a15ef84e0972e4799227cb01977859
Elena Shashina <elvehnn@yandex.ru>
2022-11-04 13:42:36 +0300

    fix: trying to fix static images for netlify

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`dist`));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
 });
 
