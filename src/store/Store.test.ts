bd9b5460970e533d735dedfb07bf3efe3486bb95
Elena Shashina <elvehnn@yandex.ru>
2022-11-08 00:01:01 +0300

    feat: add jest to the project and configure it

---
+++ src/store/Store.test.ts
@@ -0,0 +1,9 @@
+describe('Store', () => {
+  //   it('should return default state', () => {
+  //     store.setState(defaultState);
+
+  //     expect(store.getState()).toEqual(defaultState);
+  //   });
+
+  it('passes', () => expect(1).toBe(1));
+});
